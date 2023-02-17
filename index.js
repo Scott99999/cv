a5d3b86f1fc613882e2ba1574808423d3b241db9
Scott99999 <125707990+Scott99999@users.noreply.github.com>
2023-02-17 18:54:21 -0500

    Added initial website files

---
+++ index.js
@@ -0,0 +1,18 @@
+x = 2;
+y = 33;
+temp = x + y;
+console.log(temp)
+
+
+function myFunction () {
+    console.log('myFunction is running');
+}
+
+function myFunction () {
+    console.log('myFunction is running');
+}
+
+function myFunction () {
+    console.log('myFunction is running');
+}
+
